7d31835e0684be9802a1468f057477261ef1de11
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-02-28 00:43:12 -0800

    added spots

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,49 @@
-// This is our API key. Add your own API key between the ""
-var APIKey = "";
-// Here we are building the URL we need to query the database
-var queryURL = "http://api.spitcast.com/api/spot/all";
-// We then created an AJAX call
 var fTemp = null;
 var lat = null;
 var long = null;
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    console.log(queryURL);       
-    console.log(response);
-    var fTemp = (((response.main.temp - 273.15)*1.8) + 32);
-    var lat = response.coord.lat;
-    var long = response.coord.lon;
-    var wIcon = response.weather[0].icon;
-    console.log(wIcon);
-    $(".icon-output").html("<img src='"+ wIconURL+ "' >");
-    $(".city").text(response.name);  
-    $(".wind").text("Wind Speed: " + response.wind.speed);
-    $(".humidity").text("Humidity: " + response.main.humidity);  
-    $("#lat-long").text(" "+ lat + ", " + long); 
-    $("#temp-output").text(fTemp + " Degrees");
-    if (fTemp >= 70){
-        $("#temp-output").css("color", "orange");
-    };
-    if (fTemp <= 69){
-        $("#temp-output").css("color", "green");
-    };
-    if (fTemp <= 40){
-        $("#temp-output").css("color", "blue");
-    };
-});
+
+// Insert API Key if needed (not needed for spitcast)
+var APIKey = "";
+// Insert URL of API
+var queryURL = "http://api.spitcast.com/api/spot/all";
+
+// Create AJAX call
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);       
+        console.log(response);
+
+        // var fTemp = (((response.main.temp - 273.15)*1.8) + 32);
+        // var lat = response.coord.lat;
+
+        // $(".icon-output").html("<img src='"+ wIconURL+ "' >");
+        // $(".city").text(response.name);  
+        // $(".wind").text("Wind Speed: " + response.wind.speed);
+        // $(".humidity").text("Humidity: " + response.main.humidity);  
+        // $("#lat-long").text(" "+ lat + ", " + long); 
+        // $("#temp-output").text(fTemp + " Degrees");
+        console.log(response[0].spot_name);
+        // $("#spot-name-output").html(response[0].spot_name);
+
+        //create a for loop to display the info of every spot
+        for (i=0;i<response.length;i++){
+            $("#spot-name-output").append("<div class='individual-spot-section' id='spot-section-num-"+i+"'>" + response[i].spot_name + " <br>"); 
+        };
+
+
+        // if (fTemp >= 70){
+        //     $("#temp-output").css("color", "orange");
+        // };
+        // if (fTemp <= 69){
+        //     $("#temp-output").css("color", "green");
+        // };
+        // if (fTemp <= 40){
+        //     $("#temp-output").css("color", "blue");
+        // };
+
+    });
+
+
